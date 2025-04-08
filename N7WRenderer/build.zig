85f34d3ff3d6cce3f83602b83009704792c56fd8
N7Art <n7141983@gmail.com>
2025-04-08 15:51:30 +0300

    added raylib

--- N7WRenderer/build.zig
+++ N7WRenderer/build.zig
@@ -1,7 +1,19 @@
 const std = @import("std");
 pub fn build(b: *std.Build) void {
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+
+    //raylib
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
     const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -12,6 +24,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
     
     exe_mod.addImport("N7WRenderer_lib", lib_mod);
@@ -29,6 +42,9 @@ pub fn build(b: *std.Build) void {
         .root_module = exe_mod,
     });
     
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
     
     b.installArtifact(exe);
     
--- N7WRenderer/flake.nix
+++ N7WRenderer/flake.nix
@@ -30,7 +30,7 @@
 
       devShells.${system}.default = pkgs.mkShell {
 
-        buildInputs = [
+        nativeBuildInputs = [
           wayland-scanner
           wayland
           libxkbcommon
