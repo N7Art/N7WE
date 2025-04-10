f95a33bd20cda096d2cd6630d220d21f493194b8
N7Art <n7141983@gmail.com>
2025-04-10 11:31:56 +0300

    extract body to file

--- N7WFetcher/src/main.zig
+++ N7WFetcher/src/main.zig
@@ -3,6 +3,21 @@ const lib = @import("N7WFetcher_lib");
 const std = @import("std");
 
 pub fn main() !void {
-    const url = lib.get_url_from_cmd();
-    try lib.GET(url) ;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const url = lib.get_url_from_cmd(allocator);
+
+    const resp: []const u8 = try lib.GET(allocator, url);
+    defer allocator.free(resp);
+
+    const file = try std.fs.cwd().createFile("../tmp/index.html", .{ .read = true });
+    defer file.close();
+    try write(&file, resp);
 }
+fn write(file: *const std.fs.File, text: []const u8) !void {
+    try file.writeAll(text);
+}
+
+
--- N7WFetcher/src/root.zig
+++ N7WFetcher/src/root.zig
@@ -1,9 +1,8 @@
 const std = @import("std");
 const http = std.http;
-pub fn GET(url: []const u8) !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+
+/// needs to be freed
+pub fn GET(allocator: std.mem.Allocator ,url: []const u8) ![]const u8 {
 
     var client = http.Client{ .allocator = allocator };
     defer client.deinit();
@@ -11,36 +10,24 @@ pub fn GET(url: []const u8) !void {
     const uri = try std.Uri.parse(url);
     const buf = try allocator.alloc(u8, 1024 * 1024 * 4);
     defer allocator.free(buf);
+
     var req = try client.open(.GET, uri, .{
         .server_header_buffer = buf,
     });
-    defer req.deinit();
 
+    defer req.deinit();
     try req.send();
     try req.finish();
     try req.wait();
-
-    var iter = req.response.iterateHeaders();
-    while (iter.next()) |header| {
-        std.debug.print("Name:{s}, Value:{s}\n", .{ header.name, header.value });
-    }
 
     try std.testing.expectEqual(req.response.status, .ok);
 
     var rdr = req.reader();
     const body = try rdr.readAllAlloc(allocator, 1024 * 1024 * 4);
-    defer allocator.free(body);
-
-    std.debug.print("{s}", .{body});
+    return body;
 }
 
-
-pub fn get_url_from_cmd() []const u8 {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-
-    const allocator = gpa.allocator();
-
+pub fn get_url_from_cmd(allocator: std.mem.Allocator) []const u8 {
     var argsIterator = try std.process.ArgIterator.initWithAllocator(allocator);
     defer argsIterator.deinit();
 
