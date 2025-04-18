737af328c79bcaa0b259060f928368204d8a0c17
N7Art <n7141983@gmail.com>
2025-04-18 21:52:57 +0300

    added text output from file

--- N7WRenderer/src/main.zig
+++ N7WRenderer/src/main.zig
@@ -3,17 +3,13 @@ const std = @import("std");
 const lib = @import("N7WRenderer_lib");
 const rl = @import("raylib");
 
-
 pub fn main() !void {
-    const gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-    defer allocator.destroy
 
-    const window: lib.Window = .{ .width = 500, .height = 500, .title = "halo" };
-    
-    const text = "halo";
+    const window: lib.Window = .{ .width = 1000, .height = 1000, .title = "halo" };
 
+const buf = try lib.getFileContent("../tmp/index.html");
+    const text: lib.Text = .{ .text = buf };
     lib.drawText(window, text);
-    
 }
+
+test "test" {}
--- N7WRenderer/src/root.zig
+++ N7WRenderer/src/root.zig
@@ -7,7 +7,15 @@ pub const Window = struct {
     title: [:0]const u8,
 };
 
-pub fn drawText(window: Window, text: [:0]const u8) void {
+pub const Text = struct {
+    text: [:0]const u8,
+    fontSize: i32 = 14,
+    color: rl.Color = rl.Color.black,
+    posX: i32 = 0,
+    posY: i32 = 0,
+};
+
+pub fn drawText(window: Window, text: Text) void {
     rl.initWindow(window.width, window.height, window.title);
     defer rl.closeWindow();
 
@@ -17,6 +25,17 @@ pub fn drawText(window: Window, text: [:0]const u8) void {
         defer rl.endDrawing();
         std.time.sleep(100_000_000);
         rl.clearBackground(rl.Color.blue);
-        rl.drawText(text, 200, 220, 66, .black);
+        rl.drawText(text.text, text.posX, text.posY, text.fontSize, text.color);
     }
+}
+pub fn getFileContent(path: []const u8) ![:0]u8 {
+    const file: std.fs.File = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+
+    var vbuf: [1024 * 1024 * 4]u8 = undefined;
+    const s = try file.readAll(&vbuf);
+    vbuf[s] = 0;
+
+    const buf = vbuf[0..s :0];
+    return buf;
 }
